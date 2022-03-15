4ddb605eeff1804be3f0f20660a72d55214267ab
ClearList <ClearList@DESKTOP-M024CAT>
2022-03-15 09:46:17 +0700

    Добавьте файлы проекта.

---
+++ Pashkov_pr_11/View/MenuSystem.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Pashkov_pr_11.View
+{
+    /// <summary>
+    /// Логика взаимодействия для MenuSystem.xaml
+    /// </summary>
+    public partial class MenuSystem : Window
+    {
+        public MenuSystem()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAuthorization_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"Нажата кнопка \"{btnAuthorization.Content}\"");
+        }
+    }
+}
